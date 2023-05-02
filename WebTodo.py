4ae2cf9e35b21b8e5b03e5b0baa86bc0974a4d8a
Oles <oles1stepanov@gmail.com>
2023-05-02 10:18:45 +0300

    Implement adding new item

--- WebTodo.py
+++ WebTodo.py
@@ -3,10 +3,20 @@ import functions
 
 todos = functions.get_todos()
 
+def add_todo():
+    todo = st.session_state["new_todo"] + '\n'
+    todos.append(todo)
+    functions.write_todos(todos)
+
+todos = functions.get_todos()
+
 st.title("My Todo App")
 st.subheader("This is my plans")
 st.write("Increase your productivity")
 for todo in todos:
     st.checkbox(todo)
 
-st.text_input(label="", placeholder="Add new todo")
+st.text_input(label="", placeholder="Add new todo",
+              on_change=add_todo, key='new_todo')
+
+st.session_state
