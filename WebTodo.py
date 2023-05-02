4ee2cf1dfb717cad8cb42facc80a78d949668583
Oles <oles1stepanov@gmail.com>
2023-05-02 10:31:44 +0300

    Implement Pop item

--- WebTodo.py
+++ WebTodo.py
@@ -8,13 +8,18 @@ def add_todo():
     todos.append(todo)
     functions.write_todos(todos)
 
-todos = functions.get_todos()
-
 st.title("My Todo App")
 st.subheader("This is my plans")
 st.write("Increase your productivity")
-for todo in todos:
-    st.checkbox(todo)
+
+
+for index, todo in enumerate(todos):
+    checkbox = st.checkbox(todo, key=todo)
+    if checkbox:
+        todos.pop(index)
+        functions.write_todos(todos)
+        del st.session_state[todo]
+        st.experimental_rerun()
 
 st.text_input(label="", placeholder="Add new todo",
               on_change=add_todo, key='new_todo')
