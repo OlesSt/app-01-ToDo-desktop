82f94d26d50208d99243e611e9576b78112fba25
Oles <oles1stepanov@gmail.com>
2023-06-06 18:28:56 +0300

    Fix update value

--- gui.py
+++ gui.py
@@ -14,7 +14,7 @@ label = sg.Text("Type in a to do")
 input_box = sg.InputText(tooltip="Enter todo", key='todo')
 add_button = sg.Button("Add")
 list_box = sg.Listbox(values=functions.get_todos(), key='todos',
-                      enable_events=True, size=[45,10])
+                      enable_events=True, size=[45, 10])
 edit_button = sg.Button("Edit")
 complete_button = sg.Button("Complete")
 exit_button = sg.Button("Exit")
@@ -36,7 +36,7 @@ while True:
             todos.append(new_todo)
             functions.write_todos(todos)
             window['todos'].update(values=todos)
-            window['todo'].update(values='')
+            window['todo'].update(value='')
         case "Edit":
             try:
                 todo_to_edit = values['todos'][0]
